44d5758f1374c7437f0728bb55f993854d7d6710
Mark T. Holder <mtholder@gmail.com>
2014-12-20 11:31:27 +0100

    uh... that's FLAGS

--- get-dependencies-and-build.sh
+++ get-dependencies-and-build.sh
@@ -8,7 +8,7 @@ then
 fi
 cd ncl || exit
 autoreconf || exit
-CPPLAGS="-D__extern_always_inline=inline" CC=clang CXX=clang++ ./configure --prefix=$MTREE_ROOT/installed --with-constfuncs || exit
+CPPFLAGS="-D__extern_always_inline=inline" CC=clang CXX=clang++ ./configure --prefix=$MTREE_ROOT/installed --with-constfuncs || exit
 make -j4 || exit 
 make install || exit
 cd ..

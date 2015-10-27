401a12ef159f8e2a5d79f0b9ac41b4f0aff6f974
Mark T. Holder <mtholder@gmail.com>
2015-10-27 12:30:18 -0500

    extrema functions

--- src/mt_optimize_branches.cpp
+++ src/mt_optimize_branches.cpp
@@ -6,22 +6,61 @@ namespace mt {
 
 typedef std::pair<double, double> val_lnl_t;
 template<typename FUN>
-val_lnl_t maximizeScoreForSmallBrLen(FUN fun, val_lnl_t);
+val_lnl_t maximizeScoreForSmallBrLen(FUN fun, val_lnl_t better, val_lnl_t upper);
 template<typename FUN>
-val_lnl_t maximizeScoreForLargeBrLen(FUN fun, val_lnl_t);
+val_lnl_t maximizeScoreForLargeBrLen(FUN fun, val_lnl_t better, val_lnl_t lower);
 template<typename FUN>
 val_lnl_t maximizeScoreForBracketed(FUN fun, val_lnl_t lower, val_lnl_t mid, val_lnl_t upper);
 
-
+const double SAME_LN_L_TOL = 1e-04;
 template<typename FUN>
-val_lnl_t maximizeScoreForSmallBrLen(FUN fun, val_lnl_t prev) {
-    val_lnl_t best = prev;
+val_lnl_t maximizeScoreForSmallBrLen(FUN fun, val_lnl_t curr, val_lnl_t upper) {
+    val_lnl_t best = curr;
+    val_lnl_t zeroBound{0.0, fun(0.0)};
+    if (zeroBound.second > curr.second) {
+        double nextBrLen = curr.first/2.0;
+        val_lnl_t next{nextBrLen, fun(nextBrLen)};
+        if (next.second > zeroBound.second) {
+            return maximizeScoreForBracketed(fun, zeroBound, next, curr);
+        }
+        while (zeroBound.second - next.second > SAME_LN_L_TOL) {
+            curr = next;
+            next.first = next.first / 2.0;
+            next.second = fun(next.first);
+            if (next.second > zeroBound.second) {
+                return maximizeScoreForBracketed(fun, zeroBound, next, curr);
+            }
+        }
+        best = zeroBound;
+    } else {
+        return maximizeScoreForBracketed(fun, zeroBound, curr, upper);
+    }
     return best;
 }
 
+const double MAX_BR_LEN = 300.0;
 template<typename FUN>
-val_lnl_t maximizeScoreForLargeBrLen(FUN fun, val_lnl_t prev) {
-    val_lnl_t best = prev;
+val_lnl_t maximizeScoreForLargeBrLen(FUN fun, val_lnl_t curr, val_lnl_t lower) {
+    val_lnl_t best = curr;
+    val_lnl_t upperBound{MAX_BR_LEN, fun(MAX_BR_LEN)};
+    if (upperBound.second > curr.second) {
+        double nextBrLen = (curr.first + upperBound.first)/2.0;
+        val_lnl_t next{nextBrLen, fun(nextBrLen)};
+        if (next.second > upperBound.second) {
+            return maximizeScoreForBracketed(fun, lower, next, upperBound);
+        }
+        while (upperBound.second - next.second > SAME_LN_L_TOL) {
+            curr = next;
+            next.first = (next.first + upperBound.first)/2.0;
+            next.second = fun(next.first);
+            if (next.second > upperBound.second) {
+                return maximizeScoreForBracketed(fun, curr, next, upperBound);
+            }
+        }
+        best = upperBound;
+    } else {
+        return maximizeScoreForBracketed(fun, lower, curr, upperBound);
+    }
     return best;
 }
 
@@ -50,7 +89,7 @@ double maximizeLnLForBrLen(MTInstance &instance, Arc & arc, double prevScore) {
     std::cerr << "   nu = " << tiny.first << " ===> lnL = " << tiny.second << '\n';
     std::cerr << "   nu = " << small.first << " ===> lnL = " << small.second << '\n';
     if (tiny.second > small.second) {
-        soln = maximizeScoreForSmallBrLen(brLenScorer, tiny);
+        soln = maximizeScoreForSmallBrLen(brLenScorer, tiny, small);
     } else {
         const val_lnl_t mid{0.05, brLenScorer(0.05)};
         std::cerr << "   nu = " << mid.first << " ===> lnL = " << mid.second << '\n';
@@ -67,7 +106,7 @@ double maximizeLnLForBrLen(MTInstance &instance, Arc & arc, double prevScore) {
                 if (large.second > huge.second) {
                     soln = maximizeScoreForBracketed(brLenScorer, mid, large, huge);
                 } else {
-                    soln = maximizeScoreForLargeBrLen(brLenScorer, huge);
+                    soln = maximizeScoreForLargeBrLen(brLenScorer, huge, large);
                 }
             }
         }
